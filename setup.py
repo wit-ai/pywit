b8f7899bb47cd5cd857d80cd391a6269348d6bb4
patapizza <julien@wit.ai>
2014-09-29 16:51:51 -0700

    adding references

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
 wit = Extension('wit', include_dirs=['include'], library_dirs=['lib'], sources=['pywit.c'], libraries=['wit', 'sox', 'curl'])
-setup(name='PyWit SDK', version='1.0', description='Wit SDK for Python', ext_modules=[wit])
+setup(name='PyWit SDK', version='1.0', description='Wit SDK for Python', author='Julien Odent', author_email='julien@wit.ai', url='http://wit.ai', ext_modules=[wit])
