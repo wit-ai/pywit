11dc45d9c589b8fb449115eb2c0fb68fbc7acf65
Martin Raison <martinraison@gmail.com>
2014-12-02 15:20:33 +0100

    fix tabs use

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ import shutil
 
 class build(_build):
     def run(self):
-	LIBWIT_FILE = None
-	arch = platform().lower()
-	if "arm" in arch:
-		LIBWIT_FILE = "libwit-armv6.a"
-	elif "64" in arch:
-		if "darwin" in arch:
-			LIBWIT_FILE = "libwit-64-darwin.a"
-		else:
-			LIBWIT_FILE = "libwit-64-linux.a"
-	else:
-		LIBWIT_FILE = "libwit-32-linux.a"
-	print("Retrieving platform-specific libwit library... {}".format(LIBWIT_FILE))
-	if call(["curl", "-L", "-o", "libwit.a", "/".join(["https://github.com/wit-ai/libwit/releases/download/1.1.1", LIBWIT_FILE])], cwd="libwit/lib"):
-		print("[error] unable to retrieve libwit")
-		return
+        LIBWIT_FILE = None
+        arch = platform().lower()
+        if "arm" in arch:
+            LIBWIT_FILE = "libwit-armv6.a"
+        elif "64" in arch:
+            if "darwin" in arch:
+                LIBWIT_FILE = "libwit-64-darwin.a"
+            else:
+                LIBWIT_FILE = "libwit-64-linux.a"
+        else:
+            LIBWIT_FILE = "libwit-32-linux.a"
+        print("Retrieving platform-specific libwit library... {}".format(LIBWIT_FILE))
+        if call(["curl", "-L", "-o", "libwit.a", "/".join(["https://github.com/wit-ai/libwit/releases/download/1.1.1", LIBWIT_FILE])], cwd="libwit/lib"):
+            print("[error] unable to retrieve libwit")
+            return
         _build.run(self)
 
 def libraries():
