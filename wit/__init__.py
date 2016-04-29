80578d1ae8e34b732d3b2c4b42c8f48caf4c8ec1
Tony Vivoli <tvivoli@gmail.com>
2016-04-29 09:11:44 -0700

    changed default logging from stdout to stderr

--- wit/__init__.py
+++ wit/__init__.py
@@ -1,5 +1,6 @@
 from wit import Wit
 
+import sys
 import logging
 
 # Set default logging for the module. Client applications can use a custom
@@ -7,6 +8,6 @@ import logging
 logging.getLogger(__name__).setLevel(logging.INFO)
 logging.getLogger(__name__).propagate = False
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(stream=sys.stdout)
 handler.setFormatter(formatter)
 logging.getLogger(__name__).addHandler(handler)
